78228ccbc364506ec3573629191916c91f419721
B1nd00 <92019928+B1nd00@users.noreply.github.com>
2022-03-21 13:37:00 -0700

    Add files via upload

---
+++ trial1.ino
@@ -0,0 +1,94 @@
+#include <SoftwareSerial.h>
+SoftwareSerial gprsSerial(2,3);
+#include <String.h>
+
+ 
+void setup()
+{
+  gprsSerial.begin(9600);               // the GPRS baud rate   
+  Serial.begin(9600);    // the GPRS baud rate 
+  delay(1000);
+}
+ 
+void loop()
+{
+      
+  if (gprsSerial.available())
+    Serial.write(gprsSerial.read());
+
+  gprsSerial.println("AT");
+  delay(1000);
+
+  gprsSerial.println("AT+CPIN?");
+  delay(1000);
+
+  gprsSerial.println("AT+CREG?");
+  delay(1000);
+
+  gprsSerial.println("AT+CGATT?");
+  delay(1000);
+
+  gprsSerial.println("AT+CIPSHUT");
+  delay(1000);
+
+  gprsSerial.println("AT+CIPSTATUS");
+  delay(2000);
+
+  gprsSerial.println("AT+CIPMUX=0");
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CSTT=\"airtelgprs.com\"");//start task and setting the APN,
+  delay(1000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIICR");//bring up wireless connection
+  delay(3000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIFSR");//get local IP adress
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSPRT=0");
+  delay(3000);
+ 
+  ShowSerialData();
+  
+  gprsSerial.println("AT+CIPSTART=\"TCP\",\"192.168.43.30\",\"80\"");//start up the connection
+  delay(6000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSEND");//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+  
+  String str="GET /projscript/ultra1.php?depth=20&lat=30&longi=10";
+  Serial.println(str);
+  gprsSerial.println(str);//begin send data to remote server
+  gprsSerial.println(" HTTP/1.1"); 
+  gprsSerial.println("Host: 192.168.43.30"); 
+  gprsSerial.println("Connection: close"); 
+  gprsSerial.println(); 
+  gprsSerial.println(); 
+      
+  delay(4000);
+  ShowSerialData();
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSHUT");//close the connection
+  delay(100);
+  ShowSerialData();
+} 
+void ShowSerialData()
+{
+  while(gprsSerial.available()!=0)
+  Serial.write(gprsSerial.read());
+  delay(5000); 
+  
+}
